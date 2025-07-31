0a7d6d567f651caa71b43dd49daf0c8a1ebb1c8a
Davi Versan <158575755+daviversan@users.noreply.github.com>
2025-07-31 14:09:16 +0000

    update requirements

--- backend/app/utils/google_auth.py
+++ backend/app/utils/google_auth.py
@@ -1,4 +1,3 @@
-# em backend/app/utils/google_auth.py
 import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -15,30 +14,32 @@ def get_drive_service():
     token_path = os.path.join(backend_dir, "token.json")
     credentials_path = os.path.join(backend_dir, "credentials.json")
 
-    creds_json_content = os.getenv("CREDENTIALS_JSON_CONTENT")
-    token_json_content = os.getenv("TOKEN_JSON_CONTENT")
-
-    if creds_json_content:
-        with open(credentials_path, "w") as f:
-            f.write(creds_json_content)
-
-    if token_json_content:
-        with open(token_path, "w") as f:
-            f.write(token_json_content)
-
     if os.path.exists(token_path):
         creds = Credentials.from_authorized_user_file(token_path, SCOPES)
-
+    
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
-            creds = flow.run_local_server(port=0)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                credentials_path, 
+                SCOPES,
+                redirect_uri='urn:ietf:wg:oauth:2.0:oob' 
+            )
+            # Gera a URL de autorização
+            auth_url, _ = flow.authorization_url(prompt='consent')
+            
+            print('Please go to this URL and authorize access:')
+            print(auth_url)
+            
+            # Pede para o usuário colar o código
+            code = input('Enter the authorization code here: ')
+            flow.fetch_token(code=code)
+            creds = flow.credentials
 
         with open(token_path, "w") as token:
             token.write(creds.to_json())
-
+    
     try:
         service = build("drive", "v3", credentials=creds)
         print("Serviço do Google Drive autenticado com sucesso.")
